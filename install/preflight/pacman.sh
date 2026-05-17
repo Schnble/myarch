9da1e966fd84e829a041f911ccfb287b8cb907ae
Schnble <72614331+Schnble@users.noreply.github.com>
2026-05-17 21:35:59 +0200

    Add omarchy repo and keyring

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -2,6 +2,16 @@ if [[ -n ${MYARCH_ONLINE_INSTALL:-} ]]; then
   # Install build tools
   myarch-pkg-add base-devel
 
+  # Configure pacman with omarchy repo for custom packages
+  sudo cp -f ~/.local/share/myarch/default/pacman/pacman-${MYARCH_MIRROR:-stable}.conf /etc/pacman.conf
+
+  # Add omarchy keyring
+  sudo pacman-key --recv-keys 40DFB630FF42BCFFB047046CF0134EE6B0CAC571 --keyserver keys.openpgp.org
+  sudo pacman-key --lsign-key 40DFB630FF42BCFFB047046CF0134EE6B0CAC571
+
+  sudo pacman -Sy
+  myarch-pkg-add omarchy-keyring
+
   # Refresh all repos
   sudo pacman -Syyuu --noconfirm
 fi
