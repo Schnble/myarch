4c9f2e702cf3c025311a0d1fbd0afa3e99e9ad26
Schnble <72614331+Schnble@users.noreply.github.com>
2026-05-17 21:49:18 +0200

    Fix mirrorlist after pacman.conf copy

--- install/preflight/pacman.sh
+++ install/preflight/pacman.sh
@@ -4,6 +4,9 @@ if [[ -n ${MYARCH_ONLINE_INSTALL:-} ]]; then
 
   # Configure pacman with omarchy repo for custom packages
   sudo cp -f ~/.local/share/myarch/default/pacman/pacman-${MYARCH_MIRROR:-stable}.conf /etc/pacman.conf
+  
+   # Restore working mirrorlist
+  sudo bash -c 'echo "Server = https://mirror.rackspace.com/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist'
 
   # Add omarchy keyring
   sudo pacman-key --recv-keys 40DFB630FF42BCFFB047046CF0134EE6B0CAC571 --keyserver keys.openpgp.org
